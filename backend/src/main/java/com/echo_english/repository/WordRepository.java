4d95a4ee82e8744b25f888c2a93ce8911e12366e
vovantri123 <vovantri204@gmail.com>
2025-04-14 16:40:55 +0700

    fix: return null when word not found

--- backend/src/main/java/com/echo_english/repository/WordRepository.java
+++ backend/src/main/java/com/echo_english/repository/WordRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
-    Optional<Word> findByWord(String word);
+    Word findByWord(String word);
 
     List<Word> findByWordStartingWithIgnoreCase(String prefix);
 }
