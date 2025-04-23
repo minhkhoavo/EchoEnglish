5f11c4ad42ecea48ec35fc555138ddaec58f6c68
mkhoavo <v.minhkhoa123456@gmail.com>
2025-04-23 22:54:04 +0700

    build: youtube transcript

---
+++ backend/src/main/java/com/echo_english/controller/DocumentController.java
@@ -0,0 +1,20 @@
+package com.echo_english.controller;
+
+import io.github.thoroldvix.api.TranscriptContent;
+import io.github.thoroldvix.api.TranscriptRetrievalException;
+import io.github.thoroldvix.api.YoutubeTranscriptApi;
+import io.github.thoroldvix.internal.TranscriptApiFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentController {
+    @GetMapping("/youtube/{videoId}")
+    public ResponseEntity<TranscriptContent> getTranscript(@PathVariable String videoId) throws TranscriptRetrievalException {
+        YoutubeTranscriptApi youtubeTranscriptApi = TranscriptApiFactory.createDefault();
+        TranscriptContent transcript = youtubeTranscriptApi.getTranscript(videoId, "en");
+        return ResponseEntity.ok(transcript);
+    }
+}
